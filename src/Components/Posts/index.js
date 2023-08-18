4d97da17d0b221fd075808233fa52ea9d21e0f7b
Kashif <ahmed.kashif1001@gmail.com>
2023-08-18 19:42:02 +0500

    changes-1

--- src/Components/Posts/index.js
+++ src/Components/Posts/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Heart from '../../assets/Heart';
 import pic1 from "../../assets/iphone.jpg"
 import './style.css';
 
+
 function Posts() {
+
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async() => { 
+    const response = await fetch("https://fakestoreapi.com/products?limit=10")
+    const data = await response.json()
+
+    setProducts(fetchData);
+  }
+
+fetchData();
 
   return (
     <div className="postParentDiv">
@@ -12,11 +24,14 @@ function Posts() {
         <div className="heading">
           <span>Quick Menu</span>
           <span>View more</span>
-        </div>
+        </div> 
         <div className="cards">
-          <div
-            className="card"
-          >
+
+          {
+            products.map((item)=>{
+              return (
+                <>
+                <div className="card">
             <div className="favorite">
               <Heart></Heart>
             </div>
@@ -31,29 +46,38 @@ function Posts() {
               <span>Tue May 04 2021</span>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="recommendations">
-        <div className="heading">
-          <span>Fresh recommendations</span>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <div className="favorite">
-              <Heart></Heart>
+          
+          <div className="recommendations">
+            <div className="heading">
+              <span>Fresh recommendations</span>
             </div>
-            <div className="image">
-              <img src={pic1} alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">Rs. 250000</p>
-              <span className="name">iPhone 14 Pro Max</span>
-              {/* <p className="name"> YAMAHA R15V3</p> */}
-            </div>
-            <div className="date">
-              <span>10/5/2023</span>
-            </div>
-          </div>
+            <div className="cards">
+              <div className="card">
+                <div className="favorite">
+                  <Heart></Heart>
+                </div>
+                <div className="image">
+                  <img src={pic1} alt="" />
+                </div>
+                <div className="content">
+                  <p className="rate">Rs. 250000</p>
+                  <span className="name">iPhone 14 Pro Max</span>
+                 
+                </div>
+                <div className="date">
+                  <span>10/5/2023</span>
+                </div>
+              </div>
+              </div>
+              </>
+              )
+            })
+          };
+
+
+
+          
+        
         </div>
       </div>
     </div>
